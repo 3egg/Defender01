73515d7643e9f45b3e34b435f4b49b1cb3c26cfb
3egg <627096931@qq.com>
2019-10-30 21:27:14 +0800

    add base player

---
+++ Assets/Scripts/player/FirstPersonRotate.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+namespace player
+{
+    public class FirstPersonRotate: MonoBehaviour
+    {
+        public TouchField toucField;
+
+        private RigidbodyFirstPersonController fps;
+        void Start()
+        {
+            fps = GetComponent<RigidbodyFirstPersonController>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            fps.mouseLook.lookAxis = toucField.touchDist;
+        }
+    }
+}
