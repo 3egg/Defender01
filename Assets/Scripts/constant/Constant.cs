d20d598cc136afa7137013f1c37418e7deb64b83
Kevin Li (kevli) <v-kevli@expedia.com>
2019-11-02 19:16:41 +0800

    add image click obj, need to fix image music bug

--- Assets/Scripts/constant/Constant.cs
+++ Assets/Scripts/constant/Constant.cs
@@ -3,12 +3,25 @@
     public static class Constant
     {
         public const string OnUi = "OnUI/";
+        public const string SceneImg = "SceneImg/";
+        public const string Prefab = "Prefab/";
         public const string Ui = "UI";
         public const string Player = "Player";
         public const string TouchField = "TouchField";
         public const string MusicButton = "MusicButton";
+        public const string SceneButton = "SceneButton";
+        public const string SceneMenu = "SceneMenu";
+        public const string SceneLoadImg = "SceneLoadIMG";
+        public const string ImgObj = "ImgObj";
+        public const string VideoObj = "VideoObj";
         public const string Clone = "(Clone)";
         public const string First01 = "First01";
         public const string Second02 = "Second02";
+        public const string Image = "Image";
+        public const string Video = "Video";
+        public const string Word = "Word";
+        public const string Close = "Close";
+        
+        
     }
 }
--- Assets/Scripts/controller/GameController.cs
+++ Assets/Scripts/controller/GameController.cs
@@ -1,24 +1,33 @@
 ﻿using System;
 using System.Collections;
+using System.IO;
+using System.Linq;
 using System.Timers;
 using constant;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
 using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
+using UnityEngine.UI;
 using utils;
 
 namespace controller
 {
     public class GameController : MonoBehaviour
     {
+        private static GameController _instance = null;
         private AudioSource _audioSource;
-        static GameController _instance = null;
         private GameObject _player;
         private GameObject _touchField;
         private GameObject _musicBtn;
         private Transform _uiParent;
         private Transform _playerParent;
         private Transform _tfParent;
+        private Transform _sceneMenu;
+        private GameObject _sceneLoadImg;
+        private GameObject _imgObj;
+        private GameObject _videoObj;
 
         public static GameController Instance
         {
@@ -35,6 +44,102 @@ namespace controller
         {
             loadPlayer();
             loadMusic();
+            loadSceneBtns();
+            loadImageAndVideoObj();
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hitInfo;
+                if (Physics.Raycast(ray, out hitInfo))
+                {
+                    var tran = hitInfo.transform;
+                    if (tran.name.Equals("Plane")) return;
+                    //strs格式为 Image-1 或者 Video-1. 通过不同的名称打开不同的对象
+                    var strs = tran.name.Split('-');
+                    switch (strs[0])
+                    {
+                        case Constant.Image:
+                            clickImage(strs);
+                            break;
+                        case Constant.Video:
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void clickImage(string[] strs)
+        {
+            if (_audioSource.isPlaying) pauseOrPlay();
+
+            setRaycasTarget(false);
+            _imgObj.SetActive(true);
+            var closeTran = _imgObj.GetComponentsInChildren<Image>()
+                .First(t => t.name.Equals(Constant.Close)).transform;
+            closeTran.AddBtnListener(() =>
+            {
+                _imgObj.SetActive(false);
+                
+                if (!_audioSource.isPlaying) pauseOrPlay();
+
+                setRaycasTarget(true);
+            });
+            var text = Resources.Load(Constant.Word + "/" + "Text" + strs[1]) as TextAsset;
+            _imgObj.GetComponentInChildren<Text>().text = text != null ? text.text : "";
+            _imgObj.GetComponentInChildren<Image>().sprite =
+                Resources.Load<Sprite>(Constant.Image + "/" + strs[0] + strs[1]);
+        }
+
+        private void setRaycasTarget(bool isTouch)
+        {
+            _touchField.GetComponent<Image>().raycastTarget = isTouch;
+            _musicBtn.GetComponent<Image>().raycastTarget = isTouch;
+            foreach (var button in _sceneMenu.GetComponentsInChildren<Button>())
+            {
+                button.interactable = isTouch;
+            }
+        }
+
+        public void loadImageAndVideoObj()
+        {
+            if (_imgObj == null)
+            {
+                _imgObj = LoadUtil.Single.loadAndInstaniate(Constant.Prefab + Constant.ImgObj, _uiParent);
+            }
+
+            _imgObj.SetActive(false);
+            if (_videoObj == null)
+            {
+                _videoObj = LoadUtil.Single.loadAndInstaniate(Constant.Prefab + Constant.VideoObj, _uiParent);
+            }
+
+            _videoObj.SetActive(false);
+        }
+
+        public void loadSceneBtns()
+        {
+            //Use SceneManager.sceneCount and SceneManager.GetSceneAt(int index) to loop the all scenes instead.
+            var sceneCount = SceneManager.sceneCountInBuildSettings;
+            var scenes = new string[sceneCount];
+            if (_sceneMenu == null)
+            {
+                _sceneMenu = LoadUtil.Single.loadAndInstaniate(Constant.OnUi + Constant.SceneMenu, _uiParent).transform;
+            }
+
+            for (int i = 0; i < scenes.Length; i++)
+            {
+                scenes[i] = Path.GetFileNameWithoutExtension(SceneUtility.GetScenePathByBuildIndex(i));
+                var sceneName = scenes[i];
+                var sceneBtn = LoadUtil.Single.loadAndInstaniate(Constant.OnUi + Constant.SceneButton, _sceneMenu);
+                sceneBtn.GetComponent<Button>().GetComponentInChildren<Text>().text = sceneName;
+                sceneBtn.transform.AddBtnListener(() => { loadScene(sceneName); });
+            }
         }
 
         public void loadPlayer()
@@ -79,20 +184,38 @@ namespace controller
 
         public void pauseOrPlay()
         {
-            var audioSource = _instance._audioSource;
-            if (audioSource.isPlaying)
+            if (_audioSource.isPlaying)
             {
-                audioSource.Pause();
+                _audioSource.Pause();
             }
             else
             {
-                audioSource.Play();
+                _audioSource.Play();
             }
         }
 
-
+        //通过场景名称跳转到不同的场景,scenName是当前场景
         public void loadScene(string sceneName)
         {
+            if (_sceneLoadImg == null)
+            {
+                _sceneLoadImg = LoadUtil.Single.loadAndInstaniate(Constant.OnUi + Constant.SceneLoadImg, _uiParent);
+            }
+
+            var image = _sceneLoadImg.GetComponent<Image>();
+            var sprite = Resources.Load<Sprite>(Constant.SceneImg + sceneName);
+            image.sprite = sprite;
+            image.DOKill();
+            image.DOColor(new Color(1f, 1f, 1f, 1f), 1);
+            var trans = _player.GetComponentInChildren<Camera>().transform;
+            trans.DOKill();
+            trans.DOLocalMove(Vector3.forward * 5, 1);
+            StartCoroutine(loadSceneCoroutine(sceneName));
+        }
+
+        private IEnumerator loadSceneCoroutine(string sceneName)
+        {
+            yield return new WaitForSeconds(1);
             StartCoroutine(startLoading(sceneName));
         }
 
@@ -112,6 +235,8 @@ namespace controller
                 {
                     loadPlayer();
                     loadMusic();
+                    loadSceneBtns();
+                    loadImageAndVideoObj();
                     break;
                 }
             }
